712b82763a59cd13d4fb465828b53496e2b64c58
mihalo3c@gmail.com <mihalo3c@gmail.com>
2019-03-20 19:22:46 +0000

    fix3

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -33,9 +33,13 @@ class TestHelpers(object):
         
     def test_clean_hex_data(self):
         assert ethdata.clean_hex_data("2e6236591bfa37c683ce60d6cfde40396a114ff1", "address") == "0x2e6236591bfa37c683ce60d6cfde40396a114ff1"
-        assert ethdata.clean_hex_data("0xdeadbeef", "uint256") == 3_735_928_559
+        assert ethdata.clean_hex_data("0xdeadbeef", "int256") == 3_735_928_559
         assert ethdata.clean_hex_data("0xdeadbeef", "uint8") == 3_735_928_559
+        assert ethdata.clean_hex_data("0xdeadbeef", "int256") == 3_735_928_559
+        assert ethdata.clean_hex_data("0xdeadbeef", "int64") == 3_735_928_559
+        assert ethdata.clean_hex_data("0xdeadbeef", "int8") == 3_735_928_559
         assert ethdata.clean_hex_data("5343484150000000000000000000000000000000000000000000000000000000", "string") == "5343484150000000000000000000000000000000000000000000000000000000"
+		
         with pytest.warns(UserWarning):
              assert ethdata.clean_hex_data("0xdeadbeef", "unknown") == "0xdeadbeef"
 			 
